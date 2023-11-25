1b550b4ad0be73ea6682d11aed97d17f50e49e7b
piyushjojo <impiyushjoshi@gmail.com>
2023-11-26 00:20:26 +0530

    Initial Commit. Notes User Entity Added.
    Signed-off-by: piyushjojo <impiyushjoshi@gmail.com>

---
+++ src/main/java/com/app/cc/entity/Users.java
@@ -0,0 +1,31 @@
+package com.app.cc.entity;
+
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity(name = "users")
+@Data
+public class Users {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	@Column(name="user_id")
+	private Long id;
+
+	@Column(nullable = false)
+	private String username;
+	
+	@Column(nullable = false)
+	private String email;
+	
+	@Column(nullable = false)
+	private String password;
+	
+
+}
